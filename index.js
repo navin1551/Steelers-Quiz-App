cdc5a4c45804fcfdac79d8474b9f79d618df48e4
Navin Kumar <navin1551@gmail.com>
2019-07-05 21:14:37 -0700

    fixed webkit button issue

--- index.js
+++ index.js
@@ -1,111 +1,116 @@
-'use strict';
+"use strict";
 
 const questionSet = [
   {
     number: 1,
-    problem: 'How many Super Bowls have the Steelers won?',
-    choice1: '4',
-    choice2: '6',
-    choice3: '0',
-    choice4: '11',
-    answer: '6'
+    problem: "How many Super Bowls have the Steelers won?",
+    choice1: "4",
+    choice2: "6",
+    choice3: "0",
+    choice4: "11",
+    answer: "6"
   },
 
   {
     number: 2,
-    problem: 'What is the name of the stadium the Steelers play their home games at currently?',
-    choice1: 'Heinz Field',
-    choice2: 'Miners Dome',
-    choice3: 'Three Rivers Stadium',
-    choice4: 'PNC Park',
-    answer: 'Heinz Field'
+    problem:
+      "What is the name of the stadium the Steelers play their home games at currently?",
+    choice1: "Heinz Field",
+    choice2: "Miners Dome",
+    choice3: "Three Rivers Stadium",
+    choice4: "PNC Park",
+    answer: "Heinz Field"
   },
 
   {
     number: 3,
-    problem: 'The Pittsburgh Steelers have only been owned by one family. What is their last name?',
-    choice1: 'Carrol',
-    choice2: 'Knoll',
-    choice3: 'Rockefeller',
-    choice4: 'Rooney',
-    answer: 'Rooney'
+    problem:
+      "The Pittsburgh Steelers have only been owned by one family. What is their last name?",
+    choice1: "Carrol",
+    choice2: "Knoll",
+    choice3: "Rockefeller",
+    choice4: "Rooney",
+    answer: "Rooney"
   },
 
   {
     number: 4,
     problem: "Who was not a member of the feared Killer B's?",
-    choice1: 'Antonio Brown',
-    choice2: 'Chris Boswell',
-    choice3: 'Ben Roethlisberger',
+    choice1: "Antonio Brown",
+    choice2: "Chris Boswell",
+    choice3: "Ben Roethlisberger",
     choice4: "Le'Veon Bell",
-    answer: 'Chris Boswell',
+    answer: "Chris Boswell"
   },
 
   {
     number: 5,
-    problem: 'What division do the Pittsburgh Steelers play in currently?',
-    choice1: 'NFC North',
-    choice2: 'AFC East',
-    choice3: 'AFC North',
-    choice4: 'NFC East',
-    answer: 'AFC North',
+    problem: "What division do the Pittsburgh Steelers play in currently?",
+    choice1: "NFC North",
+    choice2: "AFC East",
+    choice3: "AFC North",
+    choice4: "NFC East",
+    answer: "AFC North"
   },
 
   {
     number: 6,
-    problem: 'What do the yellow, red, and blue diamonds in the teams logo stand for?',
-    choice1: 'The blood, sweat, and tears players put into the game',
-    choice2: 'Art Rooneys children’s favorite colors',
-    choice3: 'The three elements that combine to create steel',
-    choice4: 'The teams colors',
-    answer: 'The three elements that combine to create steel',
+    problem:
+      "What do the yellow, red, and blue diamonds in the teams logo stand for?",
+    choice1: "The blood, sweat, and tears players put into the game",
+    choice2: "Art Rooneys children’s favorite colors",
+    choice3: "The three elements that combine to create steel",
+    choice4: "The teams colors",
+    answer: "The three elements that combine to create steel"
   },
 
   {
     number: 7,
-    problem: 'Who beat the Pittsburgh Steelers in the 2010 Super Bowl in Arlington, Texas?',
-    choice1: 'Green Bay Packers',
-    choice2: 'New England Patriots',
-    choice3: 'Arizona Cardinals',
-    choice4: 'Dallas Cowboys',
-    answer: 'Green Bay Packers',
+    problem:
+      "Who beat the Pittsburgh Steelers in the 2010 Super Bowl in Arlington, Texas?",
+    choice1: "Green Bay Packers",
+    choice2: "New England Patriots",
+    choice3: "Arizona Cardinals",
+    choice4: "Dallas Cowboys",
+    answer: "Green Bay Packers"
   },
 
   {
     number: 8,
-    problem: 'What is the name of the yellow rally towels Steeler fans are known to carry at games and other places?',
-    choice1: 'Tomlin Towel',
-    choice2: 'Touchdown Towel',
-    choice3: 'Terrible Towel',
-    choice4: 'Turnover Towel',
-    answer: 'Terrible Towel',
+    problem:
+      "What is the name of the yellow rally towels Steeler fans are known to carry at games and other places?",
+    choice1: "Tomlin Towel",
+    choice2: "Touchdown Towel",
+    choice3: "Terrible Towel",
+    choice4: "Turnover Towel",
+    answer: "Terrible Towel"
   },
 
   {
     number: 9,
-    problem:'How many head coaches has the Steelers organization had since their creation in 1933?',
-    choice1: '3',
-    choice2: '5',
-    choice3: '6',
-    choice4: '10',
-    answer: '3',
+    problem:
+      "How many head coaches has the Steelers organization had since their creation in 1933?",
+    choice1: "3",
+    choice2: "5",
+    choice3: "6",
+    choice4: "10",
+    answer: "3"
   },
 
   {
-  number: 10,
-  problem: 'In 1943, due to depleted NFL rosters from World War 2, the Steelers merged with this Philadelphia team to become what?',
-  choice1: 'Pirates',
-  choice2: 'Redwings',
-  choice3: 'Steagles',
-  choice4: 'Seahawks',
-  answer: 'Steagles',
+    number: 10,
+    problem:
+      "In 1943, due to depleted NFL rosters from World War 2, the Steelers merged with this Philadelphia team to become what?",
+    choice1: "Pirates",
+    choice2: "Redwings",
+    choice3: "Steagles",
+    choice4: "Seahawks",
+    answer: "Steagles"
   }
 ];
 
-
-let correctNum = 0
+let correctNum = 0;
 let currentQuestion = 0;
-
 
 function quizQuestions(question) {
   return `<section>
@@ -118,60 +123,66 @@ function quizQuestions(question) {
         <fieldset>
             <legend><h2>${question.problem}</h2></legend>
               <label>
-                <input class= "input-class" type="radio" name="question" checked= 'checked' value='${question.choice1}'>
+                <input class= "input-class" type="radio" name="question" checked= 'checked' value='${
+                  question.choice1
+                }'>
                 <span>${question.choice1}</span>
               </label>
 
               <label>
-                <input class= "input-class" type="radio" name="question" value='${question.choice2}'>
+                <input class= "input-class" type="radio" name="question" value='${
+                  question.choice2
+                }'>
                 <span>${question.choice2}</span>
               </label>
 
               <label>
-                <input class= "input-class" type="radio" name="question" value='${question.choice3}'>
+                <input class= "input-class" type="radio" name="question" value='${
+                  question.choice3
+                }'>
                 <span>${question.choice3}</span>
               </label>
 
               <label>
-                <input class= "input-class" type="radio" name="question" value='${question.choice4}'>
+                <input class= "input-class" type="radio" name="question" value='${
+                  question.choice4
+                }'>
                 <span>${question.choice4}</span>
               </label>
               <input class= "submit-button" type="submit" value="submit">
         </fieldset>
     </form>
-  </section>`
-        };
+  </section>`;
+}
 
-
-function startButtonHandle(){
-  $('#start-button').on('click', function(event) {
-    $('.main').hide();
+function startButtonHandle() {
+  $("#start-button").on("click", function(event) {
+    $(".main").hide();
     let firstQuestion = questionSet[currentQuestion];
     let firstQuestionHTML = quizQuestions(firstQuestion);
-    $('#question-area').html(firstQuestionHTML);
-  })
+    $("#question-area").html(firstQuestionHTML);
+  });
 }
 
 function formButtonHandle() {
-  $('#question-area').on('submit', '#question-form', function(event) {
+  $("#question-area").on("submit", "#question-form", function(event) {
     event.preventDefault();
-    let userChoice = $('input[name=question]:checked').val();
+    let userChoice = $("input[name=question]:checked").val();
     let realAnswers = questionSet[currentQuestion].answer;
-    $('#question-area').hide();
-    $('.feedback').show();
+    $("#question-area").hide();
+    $(".feedback").show();
     if (userChoice === realAnswers) {
       correctNum++;
       startCorrectGif();
-    }
-    else{
+    } else {
       startIncorrectGif();
     }
   });
 }
 
 function resultsPage() {
-  $('#question-area').html(
-   `<section id= "finalPage">
+  $("#question-area").html(
+    `<section id= "finalPage">
     <div class='summary'>
       <h1>Are You Worthy?</h1>
       <span class= "final-score">Your Score: ${correctNum}/10</span>
@@ -180,58 +191,56 @@ function resultsPage() {
   </section>`
   );
   restartButtonHandle();
-};
+}
 
 function restartButtonHandle() {
-  $('#finalPage').on('click', '#restart-button', function(event) {
+  $("#finalPage").on("click", "#restart-button", function(event) {
     currentQuestion = 0;
     correctNum = 0;
     let firstQuestion = questionSet[currentQuestion];
     let firstQuestionHTML = quizQuestions(firstQuestion);
-    $('#question-area').html(firstQuestionHTML);
+    $("#question-area").html(firstQuestionHTML);
   });
 }
 
 function nextButtonHandle() {
-  $('.feedback').on('click', '.next-button', function(event) {
-    $('#question-area').show();
-    $('.feedback').hide();
-    if (currentQuestion < questionSet.length -1){
-      currentQuestion+=1;
+  $(".feedback").on("click", ".next-button", function(event) {
+    $("#question-area").show();
+    $(".feedback").hide();
+    if (currentQuestion < questionSet.length - 1) {
+      currentQuestion += 1;
       let nextQuestion = questionSet[currentQuestion];
       let nextQuestionHTML = quizQuestions(nextQuestion);
-      $('#question-area').html(nextQuestionHTML);
-    }
-    else {
+      $("#question-area").html(nextQuestionHTML);
+    } else {
       resultsPage();
     }
   });
 }
 
-
 function startCorrectGif() {
-  const correctGif =
-    `<section class="feedback feedback1">
+  const correctGif = `<section class="feedback feedback1">
       <h2>Correct!</h2>
       <button class= "next-button">Next</button>
-    </section>`
-  $('.feedback').html(correctGif);
-};
+    </section>`;
+  $(".feedback").html(correctGif);
+}
 
-function startIncorrectGif () {
-  const incorrectGif =
-    `<section class="feedback feedback2">
-      <h2>Wrong! The correct answer is: ${questionSet[currentQuestion].answer}</h2>
+function startIncorrectGif() {
+  const incorrectGif = `<section class="feedback feedback2">
+      <h2>Wrong! The correct answer is: ${
+        questionSet[currentQuestion].answer
+      }</h2>
       <button class= "next-button">Next</button>
-    </section>`
-  $('.feedback').html(incorrectGif);
-};
+    </section>`;
+  $(".feedback").html(incorrectGif);
+}
 
 function buttonHandles() {
-$(startButtonHandle);
-$(formButtonHandle);
-$(restartButtonHandle);
-$(nextButtonHandle);
+  $(startButtonHandle);
+  $(formButtonHandle);
+  $(restartButtonHandle);
+  $(nextButtonHandle);
 }
 
 buttonHandles();
